46071dbc98ee5879059ebced4ab42fb03034c3f2
ahmed abdelaal <ahmed.abdelaal.aly@gmail.com>
2020-03-24 16:26:17 +0200

    handling channel open

--- app/src/main/java/com/example/youtubeplayer/adpter/ChannelAdapter.java
+++ app/src/main/java/com/example/youtubeplayer/adpter/ChannelAdapter.java
@@ -1,6 +1,7 @@
 package com.example.youtubeplayer.adpter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +16,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.youtubeplayer.R;
 import com.example.youtubeplayer.models.Item;
+import com.google.android.youtube.player.YouTubeIntents;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
@@ -27,19 +29,16 @@ public class ChannelAdapter extends RecyclerView.Adapter<ChannelAdapter.Holder> 
         this.context = context;
         this.items = items;
     }
-
     @NonNull
     @Override
     public Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(context).inflate(R.layout.channel_item, parent, false);
         return new Holder(view);
     }
-
     @Override
     public void onBindViewHolder(@NonNull Holder holder, int position) {
         holder.onDataBinding(context, items.get(position));
     }
-
     @Override
     public int getItemCount() {
         return items.size();
@@ -48,31 +47,23 @@ public class ChannelAdapter extends RecyclerView.Adapter<ChannelAdapter.Holder> 
     class Holder extends RecyclerView.ViewHolder {
         ImageView channelImg;
         TextView channelName;
-        TextView channelSubscriptions;
         Button channelBtnSups;
+        Button goChannelBtn;
         boolean isSubscribed = false;
-
-
-
 
         public Holder(@NonNull View itemView) {
             super(itemView);
             channelImg = itemView.findViewById(R.id.channelLogo);
             channelName = itemView.findViewById(R.id.channelName);
             channelBtnSups = itemView.findViewById(R.id.subscribeBtn);
-          //  channelSubscriptions = itemView.findViewById(R.id.totalsup);
-
-
-
+            goChannelBtn = itemView.findViewById(R.id.goBtn);
         }
 
         public void onDataBinding(final Context context, Item item) {
+
             Picasso.with(context).load(item.getSnippet().getThumbnails()
                     .getMedium().getUrl()).into(channelImg);
             channelName.setText(item.getSnippet().getTitle());
-            //channelSubscriptions.setText(item.getStatistics().getSubscriberCount());
-
-
             channelBtnSups.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -81,13 +72,20 @@ public class ChannelAdapter extends RecyclerView.Adapter<ChannelAdapter.Holder> 
                         channelBtnSups.setBackgroundColor(Color.GRAY);
                         Toast.makeText(context, "Subscription Added", Toast.LENGTH_SHORT).show();
                         isSubscribed = true;
-
                     } else {
                         channelBtnSups.setText("subscribe");
                         channelBtnSups.setBackgroundColor(Color.RED);
                         Toast.makeText(context, "Subscription Removed", Toast.LENGTH_SHORT).show();
                         isSubscribed = false;
                     }
+                }
+            });
+
+            goChannelBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = YouTubeIntents.createChannelIntent(context, item.getId());
+                    context.startActivity(intent);
                 }
             });
         }
